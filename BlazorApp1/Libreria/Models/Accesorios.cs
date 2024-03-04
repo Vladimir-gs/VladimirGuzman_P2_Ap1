b7c0cfb6801be048181fef3608fc5200e309efbd
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-03-04 18:49:10 -0400

    Creamos nuestra clase accesorios

---
+++ BlazorApp1/Libreria/Models/Accesorios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.Models
+{
+    public class Accesorios
+    {
+        [Key] 
+        public int AccesorioId { get; set; }
+        [Required(ErrorMessage ="Este campo no puede quedar vacio")]
+        public string? Descripcion { get; set; }
+    }
+}
