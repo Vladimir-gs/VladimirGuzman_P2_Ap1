a01cd160fdde5bc5d6f8968c00eebff55d149bdb
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-03-04 19:54:31 -0400

    Creamos Nuestros controladores

---
+++ BlazorApp1/WebApplication1/Controllers/VehiculosDetallesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Libreria.Models;
+using WebApplication1.DAL;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiculosDetallesController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public VehiculosDetallesController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: api/VehiculosDetalles
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<VehiculosDetalle>>> GetVehiculosDetalle()
+        {
+            return await _context.VehiculosDetalle.ToListAsync();
+        }
+
+        // GET: api/VehiculosDetalles/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<VehiculosDetalle>> GetVehiculosDetalle(int id)
+        {
+            var vehiculosDetalle = await _context.VehiculosDetalle.FindAsync(id);
+
+            if (vehiculosDetalle == null)
+            {
+                return NotFound();
+            }
+
+            return vehiculosDetalle;
+        }
+
+        // PUT: api/VehiculosDetalles/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVehiculosDetalle(int id, VehiculosDetalle vehiculosDetalle)
+        {
+            if (id != vehiculosDetalle.DetalleId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(vehiculosDetalle).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VehiculosDetalleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/VehiculosDetalles
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<VehiculosDetalle>> PostVehiculosDetalle(VehiculosDetalle vehiculosDetalle)
+        {
+            _context.VehiculosDetalle.Add(vehiculosDetalle);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVehiculosDetalle", new { id = vehiculosDetalle.DetalleId }, vehiculosDetalle);
+        }
+
+        // DELETE: api/VehiculosDetalles/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehiculosDetalle(int id)
+        {
+            var vehiculosDetalle = await _context.VehiculosDetalle.FindAsync(id);
+            if (vehiculosDetalle == null)
+            {
+                return NotFound();
+            }
+
+            _context.VehiculosDetalle.Remove(vehiculosDetalle);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VehiculosDetalleExists(int id)
+        {
+            return _context.VehiculosDetalle.Any(e => e.DetalleId == id);
+        }
+    }
+}
---
+++ BlazorApp1/WebApplication1/Migrations/20240304231504_Inicial.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accesorios",
+                columns: table => new
+                {
+                    AccesorioId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accesorios", x => x.AccesorioId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehiculos",
+                columns: table => new
+                {
+                    VehiculoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DetalleId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    Costo = table.Column<float>(type: "REAL", nullable: false),
+                    Gastos = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehiculos", x => x.VehiculoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VehiculosDetalle",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    VehiculosId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AccesoriosId = table.Column<int>(type: "INTEGER", nullable: false),
+                    valor = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehiculosDetalle", x => x.DetalleId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accesorios");
+
+            migrationBuilder.DropTable(
+                name: "Vehiculos");
+
+            migrationBuilder.DropTable(
+                name: "VehiculosDetalle");
+        }
+    }
+}
