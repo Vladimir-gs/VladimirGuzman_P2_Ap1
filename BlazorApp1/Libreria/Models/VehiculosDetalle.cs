4eb1043358007191a9a66ccc1ece4ffcd27efb7f
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-03-04 18:54:47 -0400

    Creamos nuestra clase vehiculosDetalle

---
+++ BlazorApp1/Libreria/Models/VehiculosDetalle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.Models
+{
+    public class VehiculosDetalle
+    {
+        [Key] 
+        public int DetalleId { get; set; }
+        [ForeignKey("Vehiculos")]
+        public int VehiculosId { get; set; }
+        [ForeignKey("Accesorios")]
+        public int AccesoriosId { get; set; }
+        [Range(0, float.MaxValue, ErrorMessage ="Este valor no puede ser menor que cero")]
+        public float valor {  get; set; }
+
+    }
+}
