90e19d47662e744c3c7e6311002b5f694f8343ae
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-03-04 18:58:07 -0400

    Creamos nuestra clase Vehiculos

---
+++ BlazorApp1/Libreria/Models/Vehiculos.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Libreria.Models
+{
+    public class Vehiculos
+    {
+        [Key] 
+        public int VehiculoId { get; set; }
+        [ForeignKey("VehiculosDetalle")]
+        public int DetalleId { get; set; }
+        public DateTime Fecha { get; set; }
+        [Required(ErrorMessage ="Este campo no puede estar vacio")]
+        public string? Descripcion { get; set; }
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
+        [Range(1,float.MaxValue, ErrorMessage ="El precio no puede ser menor que 0")]
+        public float Costo { get; set; }
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
+        [Range(1, float.MaxValue, ErrorMessage = "El precio no puede ser menor que 0")]
+        public float Gastos { get; set; }
+    }
+}
