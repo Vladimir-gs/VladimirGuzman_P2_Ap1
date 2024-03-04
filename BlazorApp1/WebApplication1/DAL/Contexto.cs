69a5536b2ca9acf4d4cbbcba946be6df2b610e1a
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-03-04 19:00:44 -0400

    Creamos nuestro contexto

--- BlazorApp1/WebApplication1/DAL/Contexto.cs
+++ BlazorApp1/WebApplication1/DAL/Contexto.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Libreria.Models;
 
 namespace WebApplication1.DAL
 {
     public class Contexto: DbContext
     {
         public Contexto(DbContextOptions<Contexto> options): base(options) { }
+
+        DbSet<Vehiculos> Vehiculos { get; set;}
+        DbSet<VehiculosDetalle> VehiculosDetalle { get; set;}
+        DbSet<Accesorios> Accesorios { get; set;}
     }
 }
